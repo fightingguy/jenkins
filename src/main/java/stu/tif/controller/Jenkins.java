a010147657c7836dda9c341ab8b7bca3328610f2
flower <1377536148@qq.com>
2020-03-25 16:51:35 +0800

    2

--- src/main/java/stu/tif/controller/Jenkins.java
+++ src/main/java/stu/tif/controller/Jenkins.java
@@ -1,4 +1,12 @@
 package stu.tif.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Jenkins {
+    @GetMapping
+    public String test(){
+        return "hello jenkins";
+    }
 }
